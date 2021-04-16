0d35c6eb616c75f1bf06facbaeee086dad3aae42
SSJDeathSpawn <adikesavan28@gmail.com>
2021-04-16 12:20:12 +0530

    The beginnings of a text renderer

---
+++ src/RPG/graphics_helper.py
@@ -0,0 +1,33 @@
+#graphics_helper.py
+import pygame
+
+class TextRenderer:
+	requests = []
+	def __init__(self):
+		pygame.font.init()
+		main_font = pygame.font.Font('../../res/fonts/EightBitDragon-anqx.ttf', 28)
+
+	#Adds a text message to the rendering queue
+	def render(self, text:str, x:int, y:int, speed=1):
+		requests.append({
+			"message": text,
+			"x": x,
+			"y": y
+			"str": "",
+			"speed": speed,
+			"background": (0,0,0),
+			"foreground": (255,255,255)
+		})
+
+	#Return an array with all the surfaces to draw as well as their transforms(?)
+	def get_render_requests(self):
+		display = []
+		for i in range(len(self.requests)):
+			if len(self.requests[i]["str"]) < len(self.requests[i]["message"]):
+				self.requests[i]["str"] = self.requests[i]["message"][:len(self.requests[i]["str"])-1+self.requests[i]["speed"]] 
+			text = main_font.render(self.requests[i]["str"], True, self.requests[i]["foreground"], self.requests[i]["background"])
+			display.append({
+				"surface": text, 
+				"object": text.get_rect()
+				})
+		return display
